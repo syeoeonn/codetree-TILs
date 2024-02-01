fef66c4ca3a0d215352a88fac5ae62a00e7a711f
SeoyeonKim <147532346+syeoeonn@users.noreply.github.com>
2024-02-01 14:00:49 +0900

    Title: 달에서 무게 구하기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240201/달에서 무게 구하기/weight-on-the-moon.py
@@ -0,0 +1,4 @@
+weight_c, ratio_m = 13, 0.165
+# print("%d * %.6f = %.6f" %(weight_c, ratio_m, weight_c*ratio_m))
+# print("{0} * {1:.6f} = {2:.6f}".format(weight_c, ratio_m, weight_c*ratio_m))
+print(f"{weight_c} * {ratio_m:.6f} = {weight_c*ratio_m:.6f}")
