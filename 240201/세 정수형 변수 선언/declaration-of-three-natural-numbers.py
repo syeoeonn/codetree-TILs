a51e97b99957be7418041e4464c2c3fcfb1f9673
SeoyeonKim <147532346+syeoeonn@users.noreply.github.com>
2024-02-01 13:20:54 +0900

    Title: 세 정수형 변수 선언, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/세 정수형 변수 선언/declaration-of-three-natural-numbers.py
+++ 240201/세 정수형 변수 선언/declaration-of-three-natural-numbers.py
@@ -1,3 +1,4 @@
 a, b, c = 7, 23, 30
 # print("{0} + {1} = {2}".format(a, b, c))
-print("{new_a} + {new_b} = {new_c}".format(new_a=a, new_b=b, new_c=c))
+# print("{new_a} + {new_b} = {new_c}".format(new_a=a, new_b=b, new_c=c))
+print(f"{a} + {b} = {c}")
