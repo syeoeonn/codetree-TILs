38fcba73c855c7aaca5355393072fabaef9b0846
SeoyeonKim <147532346+syeoeonn@users.noreply.github.com>
2024-02-01 13:42:27 +0900

    Title: 변수 출력하기 3, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240201/변수 출력하기 3/outputing-variables-3.py
@@ -0,0 +1,10 @@
+#a = 1; b = 2; c = "C"
+a, b, c = 1, 2, 'C'
+# print(a, end='')
+# print("->", end='')
+# print(b, end='')
+# print('->', end='')
+# print(c, end='')
+# print("%d->%d->%c" %(a, b, c))
+print("{0}->{1}->{2}".format(a, b, c))
+# print(f"{a}->{b}->{c}")
