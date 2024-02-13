ec07bb2c532539dc89604aba959c58a513ad1290
SeoyeonKim <147532346+syeoeonn@users.noreply.github.com>
2024-02-13 19:21:16 +0900

    Title: 둘 중 하나의 배수, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/둘 중 하나의 배수/multiple-of-either.py
+++ 240213/둘 중 하나의 배수/multiple-of-either.py
@@ -1,5 +1,6 @@
 a = int(input())
-if a % 3 == 0 or a % 5 == 0:
-    print(1)
-else:
-    print(0)
+# if a % 3 == 0 or a % 5 == 0:
+#     print(1)
+# else:
+#     print(0)
+print(int(a % 3 == 0 or a % 5 == 0))
