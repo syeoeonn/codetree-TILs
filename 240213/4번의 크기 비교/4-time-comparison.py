66ff4240873e14ee7f800b86ae86ae4efb1c1d1a
SeoyeonKim <147532346+syeoeonn@users.noreply.github.com>
2024-02-13 16:26:36 +0900

    Title: 4번의 크기 비교, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/4번의 크기 비교/4-time-comparison.py
+++ 240213/4번의 크기 비교/4-time-comparison.py
@@ -1,6 +1,8 @@
 a = int(input())
 b, c, d, e = map(int, input().split())
-print(int(a > b))
-print(int(a > c))
-print(int(a > d))
-print(int(a > e))
+# print(int(a > b))
+# print(int(a > c))
+# print(int(a > d))
+# print(int(a > e))
+for i in [b, c, d, e]:
+    print(int(a>i))
