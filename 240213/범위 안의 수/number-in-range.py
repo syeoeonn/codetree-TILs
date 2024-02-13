26266f0305bbbf283111470e7b08879cd3ccfe94
SeoyeonKim <147532346+syeoeonn@users.noreply.github.com>
2024-02-13 17:44:13 +0900

    Title: 범위 안의 수, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/범위 안의 수/number-in-range.py
+++ 240213/범위 안의 수/number-in-range.py
@@ -1,5 +1,6 @@
 a = int(input())
-if 10 <= a and a <= 20:
-    print("yes")
-else:
-    print("no")
+# if 10 <= a and a <= 20:
+#     print("yes")
+# else:
+#     print("no")
+print("yes" if 10 <= a and a <= 20 else "no")
