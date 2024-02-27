612fdeed1fcc36ef01f9a10701caf84c951058b2
SeoyeonKim <147532346+syeoeonn@users.noreply.github.com>
2024-02-27 16:16:34 +0900

    Title: 두 사람, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240227/두 사람/two-person.py
@@ -0,0 +1,7 @@
+a_n, a_s = input().split()
+b_n, b_s = input().split()
+a_n, b_n = int(a_n), int(b_n)
+if (a_n >- 19 and a_s == 'M') or (b_n >- 19 and b_s == 'M'):
+    print(1)
+else:
+    print(0)
